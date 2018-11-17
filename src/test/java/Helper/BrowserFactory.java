44d727f27fbd8ff35f197b4c9450b91fb5439db6
Aaron Tracy <atracy@gmail.com>
2018-11-17 12:39:11 -0700

    Initial commit

---
+++ src/test/java/Helper/BrowserFactory.java
@@ -0,0 +1,15 @@
+package Helper;
+
+import java.sql.Driver;
+
+public class BrowserFactory {
+
+
+    void BrowserFactory(driver){
+
+        System.out.Println("Browser Created");
+        return driver;
+    }
+
+
+}
---
+++ src/test/java/com/pages/skiutah/Challenge1OpenSkiUtah.java
@@ -0,0 +1,11 @@
+package com.pages.skiutah;
+
+import org.testng.annotations.Test;
+
+public class Challenge1OpenSkiUtah {
+
+    @Test
+    void OpenSkiUtah(){
+        System.out.println("Ski Utah Opened");
+    }
+}
