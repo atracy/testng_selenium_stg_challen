1b914a81f4c9678409e7489eae751b992c77bb49
Aaron Tracy <atracy@gmail.com>
2018-11-17 18:39:28 -0700

    Completed Test Challenge 3

--- src/test/java/com/pages/skiutah/TopNavigation.java
+++ src/test/java/com/pages/skiutah/TopNavigation.java
@@ -2,6 +2,8 @@ package com.pages.skiutah;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 
 public class TopNavigation {
@@ -12,9 +14,26 @@ public class TopNavigation {
         this.driver=ldriver;
     }
 
-    public void navigateTo(String linkText) {
-        String menuSelector = "a[class*='SuperfishMegaMenu-topLink'][title='" + linkText + "']";
-        driver.findElement(By.cssSelector(menuSelector)).click();
-        driver.findElement(By.cssSelector(menuSelector)).click();
+    public void navigateTo(String menu) {
+        this.hoverMenu(menu);
+        this.hoverMenu(menu);
+    }
+
+    public void hoverMenu(String menu) {
+        By menuSelector = By.cssSelector("a[class*='SuperfishMegaMenu-topLink'][title='" + menu + "']");
+        this.hover(menuSelector);
+    }
+
+    public void navigateToSubMenu(String menu, String subMenu){
+        this.hoverMenu(menu);
+        this.hover(By.linkText(subMenu));
+    }
+
+    private void hover(By menuItem){
+        Actions action = new Actions(driver);
+        WebElement Element = driver.findElement(menuItem);
+        action.moveToElement(Element);
+        action.click();
+        action.perform();
     }
 }
--- src/test/java/com/tests/skiutah/Challenge1OpenSkiUtah.java
+++ src/test/java/com/tests/skiutah/Challenge1OpenSkiUtah.java
@@ -22,21 +22,35 @@ public class Challenge1OpenSkiUtah {
         this.driver = BrowserFactory.startBrowser("chrome", "http://skiutah.com");
     }
 
-//    @Test(priority=1)
-//    public void AutomationChallenge1(){
-//        System.out.println("Ski Utah Opened");
-//        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
-//        homePage.checkPageTitle("Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah");
-//
-//    }
+    //Challenge 1
+    @Test(priority=1, enabled = false)
+    public void AutomationChallenge1(){
+        System.out.println("Ski Utah Opened");
+        HomePage homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.checkPageTitle("Ski Utah | Utah Ski Resorts, Lift Tickets, Ski Passes, Maps & More! - Ski Utah");
 
-    @Test(priority=2)
+    }
+
+    //Challenge 2
+    @Test(priority=2, enabled = false)
     //click on deals page
     public void AutomationChallenge2(){
         String navigateTo = "Deals";
         TopNavigation topNavigation = PageFactory.initElements(driver, TopNavigation .class);
         DealsPage dealsPage = PageFactory.initElements(driver, DealsPage.class);
         topNavigation.navigateTo(navigateTo);
+        dealsPage.VerifyOnDealsPage();
+    }
+
+    //Challenge 3
+    @Test(priority=3, enabled = false)
+    //click on deals page
+    public void AutomationChallenge3(){
+        String navigateTo = "Deals";
+        String subMenuNavigateTo = "All Deals";
+        TopNavigation topNavigation = PageFactory.initElements(driver, TopNavigation .class);
+        DealsPage dealsPage = PageFactory.initElements(driver, DealsPage.class);
+        topNavigation.navigateToSubMenu(navigateTo, subMenuNavigateTo);
         dealsPage.VerifyOnDealsPage();
     }
 
